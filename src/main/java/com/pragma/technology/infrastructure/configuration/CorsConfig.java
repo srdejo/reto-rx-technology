5d0f338cbd8fe10ec0aa3a393417c85b0c6b243d
Daniel Jimenez <srdejo@gmail.com>
2026-09-22 21:36:26 -0500

    feat: configure cors

---
+++ src/main/java/com/pragma/technology/infrastructure/configuration/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.pragma.technology.infrastructure.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+@Configuration
+public class CorsConfig implements WebFluxConfigurer {
+
+    @Value("${allowed-origins}")
+    private String allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
